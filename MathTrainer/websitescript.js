bdc8555b383097ce08b2af056b2e6cef6551eda5
Wokman <walker.whitehouse@gmail.com>
2018-04-04 09:05:35 -0400

    tard gaurd

--- MathTrainer/websitescript.js
+++ MathTrainer/websitescript.js
@@ -192,4 +192,5 @@ Your answer is incorrect. We'll show you this problem again a little later. \
     if (c == 0){
       alert("Error: We don't have enough problems on this topic difficulty");
     }
+    alert("nut: " + c);
   }
