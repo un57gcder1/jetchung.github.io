943dca58a6ae89ec2712880da0c02d4f5c1a6996
Jet Chung <jetchung1@gmail.com>
2019-06-12 11:59:46 -0400

    final fractals

--- fractals/script.js
+++ fractals/script.js
@@ -1,21 +1,20 @@
+//define some constants
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-var offset = 30;
+var offset = 40;
+var prev = [offset, offset],
+  curr;
 
-
+//calculating coordinates of the hilbert curve
 function last2bits(x) {
   return (x & 3);
 }
 
 function hindex2xy(hindex, N) {
   var positions = [
-    /* 0: */
     [0, 0],
-    /* 1: */
     [0, 1],
-    /* 2: */
     [1, 1],
-    /* 3: */
     [1, 0]
   ];
 
@@ -57,75 +56,7 @@ function hindex2xy(hindex, N) {
   return [x, y];
 }
 
-
-var prev = [offset, offset],
-  curr;
-
-function reet(i, N) {
-  ctx.fillStyle = HSVtoHex(((i / (N * N))), 1, .5);
-
-  //console.log(255-255*i/(N*N));
-  curr = hindex2xy(i, N);
-  //ctx.fillRect(20, 20, 150, 100);
-  //ctx.fillRect(30,520,50,50);
-
-  var size = 600 / (N - 1);
-
-  var x1 = prev[0] * size;
-  var y1 = (prev[1] * size);
-  var x2 = Math.abs(size * (curr[0]));
-  var y2 = Math.abs(size * (curr[1]));
-  ctx.fillRect(offset + x1, offset + y1, 5, 5);
-  if (y1 == y2 && x2 > x1) {
-    //console.log("right");
-    ctx.fillRect(offset + x1, offset + y1, x2 - x1, 5);
-  }
-  if (y1 == y2 && x2 < x1) {
-    //console.log("left");
-    ctx.fillRect(offset + x2, offset + y2, x1 - x2, 5);
-
-
-  }
-  if (x1 == x2 && y1 > y2) {
-    // console.log("up");
-    ctx.fillRect(offset + x2, offset + y2, 5, y1 - y2 + 5);
-
-  }
-  if (x1 == x2 && y1 < y2) {
-    // console.log("down");
-    //ctx.fillRect(x2,y2,50,y2-1);
-
-    ctx.fillRect(offset + x2, offset + y2, 5, y1 - y2);
-
-  }
-  ctx.beginPath();
-  ctx.stroke();
-  console.log(i);
-  //console.log(x1,y1);
-  prev = curr;
-
-}
-
-function makeAHomie() {
-  var ctx = c.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  var n = document.getElementById("reet1").value;
-  var N = Math.pow(2, n);
-
-  ctx.beginPath();
-  ctx.stroke();
-  //reet(4,16);
-  //reet(5,16);
-  //reet(6,16);
-  for (var i = 0; i < N * N; i += 1) {
-    //sleep(100);
-    reet(i, N);
-
-
-  }
-}
-
+//colors
 function HSVtoHex(h, s, v) {
   var r, g, b, i, f, p, q, t;
   if (arguments.length === 1) {
@@ -159,29 +90,99 @@ function HSVtoHex(h, s, v) {
   return "#" + componentToHex(Math.round(r * 255)) + componentToHex(Math.round(g * 255)) + componentToHex(Math.round(b * 255));
 
 }
-
 function componentToHex(c) {
   var hex = c.toString(16);
   return hex.length == 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+//helper function to sleep
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
+//helper function to draw lines (also animations)
+function drawLine(x1, y1, x2, y2, ratio) {
+  if (y1 == y2 && x2 > x1) {
+    //console.log("right");
+    ctx.fillRect(offset + x1, offset + y1, ratio * (x2 - x1), 5);
+  }
+  if (y1 == y2 && x2 < x1) {
+    //console.log("left");
+    ctx.fillRect(offset + x2, offset + y2, ratio * (x1 - x2), 5);
+
+
+  }
+  if (x1 == x2 && y1 > y2) {
+    // console.log("up");
+    ctx.fillRect(offset + x2, offset + y2, 5, ratio * (y1 - y2 + 5));
+
+  }
+  if (x1 == x2 && y1 < y2) {
+    // console.log("down");
+    //ctx.fillRect(x2,y2,50,y2-1);
+
+    ctx.fillRect(offset + x2, offset + y2, 5, ratio * (y1 - y2));
+
   }
 }
 
-function weReallyOutHere() {
-  const ctx = canvas.getContext('2d');
+//function to draw each node
+async function reet(i, N) {
+
+  ctx.fillStyle = HSVtoHex(((i / (N * N))), 1, .5);
+  var ratio = 1;
+  //console.log(255-255*i/(N*N));
+  curr = hindex2xy(i, N);
+  //ctx.fillRect(20, 20, 150, 100);
+  //ctx.fillRect(30,520,50,50);
+  var size = 600 / (N - 1);
+  var x1 = prev[0] * size;
+  var y1 = (prev[1] * size);
+  var x2 = Math.abs(size * (curr[0]));
+  var y2 = Math.abs(size * (curr[1]));
+  //drawLine(x1,y1,x2,y2,0,HSVtoHex(((i / (N * N))), 1, 1))
+
+  ctx.moveTo(x1, y1);
+  sleep(10).then(() => {
+    ctx.fillRect(offset + x1, offset + y1, 5, 5);
+
+    ctx.fillStyle = HSVtoHex(((i / (N * N))), 1, .5);
+    drawLine(x1, y1, x2, y2, 1);
+
+  });
+
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.stroke();
+  //console.log(x1,y1);
+  prev = curr;
+
+  //sleep(100);
+}
+
+function makeAHomie() {
+  var ctx = c.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  var n = document.getElementById("reet1").value;
+  var N = Math.pow(2, n);
+
+  ctx.beginPath();
+  ctx.stroke();
+  //reet(4,16);
+  //reet(5,16);
+  //reet(6,16);
+  for (var i = 0; i < N * N; i += 1) {
+    //sleep(100);
+    reet(i, N);
+    // if (ratio<1) {
+    //   requestAnimationFrame(function() {
+    //     reet(i, N, .5);
+    //     ratio += .1;
+    //   });
+    // }
 
 
-  ctx.restore();
-
+  }
 }
