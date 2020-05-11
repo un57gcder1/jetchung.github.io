fd39e38b45556e5297e275cdb6029d947cea2c89
Jet Chung <jetchung1@gmail.com>
2020-05-11 16:17:15 -0400

    grading

---
+++ grading/main.js
@@ -0,0 +1,48 @@
+var classes = {
+    "m1": "Math Competitions I",
+    "m2": "Math Competitions II",
+    "pm": "Physics: Mechanics",
+    "li": "Linguistics",
+    "as": "Astronomy",
+    "bi": "Biology"
+}
+var weeks = {
+    "w1" : "Week 1",
+    "w2" : "Week 2",
+    "w3" : "Week 3",
+    "w4" : "Week 4",
+    "w5" : "Week 5",
+}
+var days = {
+    "d1" : "Week 1",
+    "d2" : "Week 2",
+    "d3" : "Week 3",
+    "d4" : "Week 4",
+    "d5" : "Week 5",
+}
+var lookup = {
+    "35149m1w1d3": "blah"
+
+    }
+
+function addAssignment(){
+    var items = document.getElementById("gradingtodo");
+    var id = "35149m1w2d3"
+    items.innerHTML += "<ol><button onclick = displayData('" + id + "')>" + getDatafromID(id) + "</button></ol>";
+
+}
+
+
+function getDatafromID(id){
+    
+    return "Student " + id.slice(0,5) + " " + classes[id.slice(5,7)] + " " + weeks[id.slice(7,9)] + " " + days[id.slice(9,11)]
+    
+}
+
+
+function displayData(id){
+    document.getElementById("student").innerText = "Student: " + id.slice(0,5)
+    document.getElementById("assignment").innerText = "Assignment: " + classes[id.slice(5,7)] + " " + weeks[id.slice(7,9)] + " " + days[id.slice(9,11)]
+    console.log(getDatafromID("35149m1w2d3"))
+
+}
