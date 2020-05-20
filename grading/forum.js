014870d5c35edc0067a221ce1288742529d16f8c
Jet Chung <jetchung1@gmail.com>
2020-05-20 16:52:20 -0400

    add stuff

--- grading/forum.js
+++ grading/forum.js
@@ -1,33 +1,86 @@
-post = document.getElementById("post");
+const post = document.getElementById("post");
+var user = "Jet Chung";
 
 function bold() {
-    post.value += "[b][/b]"
+    post.value += "[b][/b]";
 }
 
 function underline() {
-    post.value += "[u][/u]"
+    post.value += "[u][/u]";
 }
 
 function italic() {
-    post.value += "[i][/i]"
+    post.value += "[i][/i]";
 }
 
 
 function color(c) {
-    post.value += "[color=" + c + "][/color]"
+    post.value += "[color=" + c + "][/color]";
 }
 
-function hide(){
-    post.value += "[hide][/hide]"
-
-}
-
-function url(){
-    post.value += "[url][/url]"
+function hide() {
+    post.value += "[hide][/hide]";
 
 }
 
-function img(){
-    post.value += "[img][/img]"
+function url() {
+    post.value += "[url][/url]";
+
+}
+
+function img() {
+    post.value += "[img][/img]";
+
+}
+
+
+
+function submitPost() {
+    var date = new Date()
+    date = date.getTime()
+    const dateTimeFormat = new Intl.DateTimeFormat('en', {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit'
+    })
+    const [{
+        value: month
+    }, , {
+        value: day
+    }, , {
+        value: year
+    }] = dateTimeFormat.formatToParts(date)
+
+    date = `${month} ${day}, ${year }`;
+    subject = document.getElementById("subject").value;
+    content = document.getElementById("post").value;
+
+    if (subject.length < 8 || subject.length > 50 ) {
+        document.getElementById("subject-error").style = "";
+    }
+    else {
+
+        document.getElementById("subject-error").style = "display:none;";
+
+    }
+
+    if (content.length < 8 || content.length > 10000 ) {
+        document.getElementById("post-error").style = "";
+    }
+    else {
+
+        document.getElementById("post-error").style = "display:none;";
+
+    }
+    if (subject != "" && content != "") {
+        addPost(user, subject, date);
+    }
+
+}
+
+function addPost(name, subject, date) {
+    var posts = document.getElementById("posts");
+    $(posts).prepend('<br><div class="card"><div class="card-header alert-primary"><ul class="nav navbar nav-pills card-header-pills"><li class="nav-item"><h5 class="nav navbar-text">' +
+        subject + '</h5></li><li class="nav-item"><h6>Posted by ' + name + ' on ' + date + '</h6></li></ul></div></div><br>');
 
 }
